3cf868257415da07316339108ecaff7ceb54ae68
Ricardo R <rretamal.dev@gmail.com>
2026-04-15 16:47:39 -0400

    Initial files

---
+++ MauiLLM/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiLLM
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
